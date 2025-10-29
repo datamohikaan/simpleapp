a21971da48c5e4146e07949435b5c591af546fd5
Peter Bosch <bosch.peter@icloud.com>
2025-10-29 22:31:22 +0100

    Tahoe

--- tt.py
+++ tt.py
@@ -1,8 +1,12 @@
+# pylint: disable=missing-final-newline
+
+
 def print_hello_world(name):
     print("Hello", name, "!!")
+
 
 # Calling the function
 print_hello_world("Alice")
 
 # Hello Alice !!!! commit
-## ff
+# ff
