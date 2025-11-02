279b08677d510ee07aa33a022d2b5182f265d0b6
Peter Bosch <bosch.peter@icloud.com>
2025-11-02 22:41:06 +0100

    Tahoe

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,19 +1,22 @@
 import pytest
 import os
+
 # Configure directories
+# tt
 from pathlib import Path
 
-root = Path(__file__).parent / 'test_files/files/'
+root = Path(__file__).parent / "test_files/files/"
 print(root)
-empty_folder = str(root / 'empty_folder')
+empty_folder = str(root / "empty_folder")
 print(empty_folder)
 
-@pytest.fixture(scope='function')
+
+@pytest.fixture(scope="function")
 def before_and_after():
     print("before")
 
-class TestTheTests:
 
+class TestTheTests:
     def test_1(self, before_and_after):
-        print('cwd: ' + os.getcwd())
+        print("cwd: " + os.getcwd())
         assert ((1 + 1) == 2) is True
