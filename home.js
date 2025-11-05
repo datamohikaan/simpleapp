a8a8d01fe51286af58f0893eece98ab458d5dd1c
Peter Bosch <bosch.peter@icloud.com>
2025-11-05 19:49:08 +0100

    Tahoe

---
+++ home.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(function () {
+    console.log("hello js");
+    let url = new URL(window.location.href);
+    if (!(["/over"].includes(url.pathname))){
+        console.log('We are not in over, we are in: ' + url.pathname);
+        window.history.replaceState(null, "", url.pathname);
+    }
+    console.log("we zijn in document ready van mb-functies en dit is usermenu: " + usermenu);
+    })
+  });
